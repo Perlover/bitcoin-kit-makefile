07113e1dd9bb31ce087af24bad8e38e8747ed97e
Perlover <perlover@perlover.com>
2017-11-21 18:53:40 +0100

    Added python3 for ElectrumX

---
+++ mk/python3.mk
@@ -0,0 +1,14 @@
+Python-3.6.3.tgz:
+	wget 'https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz'
+	echo 'e9180c69ed9a878a4a8a3ab221e32fa9  Python-3.6.3.tgz'|md5sum --check - || \
+		{ \
+			mv Python-3.6.3.tgz Python-3.6.3.bad.tgz &&\
+			echo "Bad automake md5 sum"; false;\
+		}
+
+python3_install: | bash_profile_install autotools_install gcc_install autoconf_install pkg-config_install Python-3.6.3.tgz
+	tar xzf Python-3.6.3.tgz
+	cd Python-3.6.3 && { \
+		./configure --prefix=$$HOME && make test && make install && echo "The python3 was installed - OK"; \
+	} &> make_out.txt && tail make_out.txt
+	@touch $@
