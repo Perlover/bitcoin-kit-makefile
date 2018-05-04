1b8572614666b6ea945c2c33a766e6980088babc
Perlover <perlover@perlover.com>
2018-05-04 19:58:04 +0200

    fix

--- mk/nodejs.mk
+++ mk/nodejs.mk
@@ -11,6 +11,6 @@ nodejs_install: |\
     node-v10.0.0.tar.gz
 	tar xzf node-v10.0.0.tar.gz
 	cd node-v10.0.0 && { \
-		LANG=C ./configure --prefix=$HOME && $(MAKE_COMPILE) && make install && echo "The node.js was installed - OK"; \
+		LANG=C ./configure --prefix=$$HOME && $(MAKE_COMPILE) && make install && echo "The node.js was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
