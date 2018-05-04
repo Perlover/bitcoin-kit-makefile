62b0fd5434d3e72853e7258cbe0311008c3df398
Perlover <perlover@perlover.com>
2018-05-04 19:47:37 +0200

    ...

--- mk/c-lightning.mk
+++ mk/c-lightning.mk
@@ -1,12 +1,9 @@
-external/c-lightning:
-	git submodule update --init --recursive
-
 lightning_install: |\
-    external/c-lightning\
+    git_submodule_install\
     python2_install\
     python3_install\
     sqlite3_install\
-    binutils_install\
+    binutils_install
 	cd external/c-lightning && { \
 		$(MAKE_COMPILE) prefix=$$HOME install && echo "The python3 was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
--- mk/common.mk
+++ mk/common.mk
@@ -6,6 +6,9 @@ bash_profile_install: | $(HOME)/.bitcoin_envs
 	echo $$'\n. $(HOME)/.bitcoin_envs' >> $(HOME)/.bash_profile
 	touch $@
 
+git_submodule_install:
+	git submodule update --init --recursive
+
 required_for_configure_install: \
     bash_profile_install\
     autotools_install\
