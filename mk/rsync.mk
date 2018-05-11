7406ba0daaf4fb60a5bc41c0a985dd2a0bd401b3
Perlover <perlover@perlover.com>
2018-05-11 16:35:55 +0200

    fix rsync. Now tested

--- mk/rsync.mk
+++ mk/rsync.mk
@@ -5,7 +5,7 @@ rsync_main_repo:
 	if [ $$RETCODE -eq 0 -o $$RETCODE -eq 23 ]; then true; else false;  fi
 
 rsync_submodules:
-	git submodule --quiet foreach --recursive 'git ls-files|''awk '\''{print "'\''$$path'\''/"$$1}'\' | rsync -dlptgoDvz --files-from=- . $(BITCOIN_TOOLS_RSYNC_SERVER_AND_PATH)
+	git submodule --quiet foreach --recursive 'git ls-files|''awk '\''{print "'\''$$toplevel/$$path'\''/"$$1}'\' | rsync -dlptgoDvz --files-from=- / $(BITCOIN_TOOLS_RSYNC_SERVER_AND_PATH)
 	RETCODE=$$?; echo $$RETCODE;\
 	if [ $$RETCODE -eq 0 -o $$RETCODE -eq 23 ]; then true; else false;  fi
 
