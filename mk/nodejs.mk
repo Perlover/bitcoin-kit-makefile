a60c83d12d5d26dab82191944521e8b4e3be71c2
Perlover <perlover@perlover.com>
2018-05-17 18:52:50 +0200

    nodejs v10.1.0

--- mk/nodejs.mk
+++ mk/nodejs.mk
@@ -1,6 +1,6 @@
-node-v10.0.0.tar.gz:
+node-v10.1.0.tar.gz:
 	gpg --keyserver pool.sks-keyservers.net --recv-keys DD8F2338BAE7501E3DD5AC78C273792F7D83545D
-	wget 'https://nodejs.org/dist/v10.0.0/$@' && wget -O $@-SHASUMS256.txt.asc 'https://nodejs.org/dist/v10.0.0/SHASUMS256.txt.asc' &&\
+	wget 'https://nodejs.org/dist/v10.1.0/$@' && wget -O $@-SHASUMS256.txt.asc 'https://nodejs.org/dist/v10.1.0/SHASUMS256.txt.asc' &&\
 	grep $@ $@-SHASUMS256.txt.asc | sha256sum -c - || { echo "Bad sign of $@"; false; }
 
 # LANG=C ./configure... for correct version of assembler
@@ -8,9 +8,9 @@ nodejs_pre_install: |\
     required_for_configure_install\
     binutils_install\
     python2_install\
-    node-v10.0.0.tar.gz
-	tar xzf node-v10.0.0.tar.gz
-	cd node-v10.0.0 && { \
+    node-v10.1.0.tar.gz
+	tar xzf node-v10.1.0.tar.gz
+	cd node-v10.1.0 && { \
 		LANG=C ./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) install && echo "The node.js was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
