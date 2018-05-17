ff40817a12a82e151ff1795e4c953561c1c3e53a
Perlover <perlover@perlover.com>
2018-05-17 17:53:07 +0200

    fix lnd's build target workaround

--- mk/lnd.mk
+++ mk/lnd.mk
@@ -2,7 +2,7 @@ lnd_install: |\
     golang_install
 	go get -d github.com/lightningnetwork/lnd &&\
 	cd $$GOPATH/src/github.com/lightningnetwork/lnd &&\
-	$(MAKE_COMPILE) && $(MAKE) install
+	$(MAKE) && $(MAKE) install
 	@touch $@
 
 btcd_install: |\
