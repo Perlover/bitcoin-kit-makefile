66b5f415b32f58b93ce507d3d534fcb4c5a7422d
Perlover <perlover@perlover.com>
2018-05-24 21:22:00 +0200

    added 'l' as alias for mainnet-lncli command (lncli for mainnet)

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -1,2 +1,3 @@
 alias testnet-lncli="lncli --macaroonpath $HOME/.lnd/admin-testnet.macaroon --rpcserver localhost:10010"
 alias mainnet-lncli="lncli --macaroonpath $HOME/.lnd/admin-mainnet.macaroon --rpcserver localhost:10009"
+alias l="mainnet-lncli"
