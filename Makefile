211a5ea367ace77dc7a3a96c0accfb5ab63f7062
Perlover <perlover@perlover.com>
2018-05-25 18:49:36 +0200

    begining of new version - don't install some packages if we have minimal required versions

--- Makefile
+++ Makefile
@@ -32,6 +32,12 @@ HASH_NETWORK_CONFIG := $(shell echo `uname  -a` `/sbin/ifconfig | awk '/inet add
 # Example: $(eval $(call COMPARE_VERSIONS,gcc -dumpversion,5.0.0,GCC_5_0_0)) after will be 'OK' or 'FAIL'
 COMPARE_VERSIONS = $(3) = $$(shell if [ "$$$$(printf '%s\n' "$(2)" $$$$($(1) &>/dev/null || echo "0.0.0") | sort -V | head -n1)" = "$(2)" ]; then echo OK; else echo FAIL; fi)
 
+VERSION_MASK := sed -r -e 's#.* ([0-9]+\.[0-9a-z]+(\.[0-9a-z]+)?)$#\1#'
+$(eval $(call COMPARE_VERSIONS,m4 --version|head -1|$(VERSION_MASK),1.4.17,M4_MIN))
+$(eval $(call COMPARE_VERSIONS,automake --version|head -1|$(VERSION_MASK),1.15,AUTOMAKE_MIN))
+$(eval $(call COMPARE_VERSIONS,autoconf --version|head -1|$(VERSION_MASK),2.69,AUTOCONF_MIN))
+$(eval $(call COMPARE_VERSIONS,libtoolize --version|head -1|$(VERSION_MASK),2.4.6,LIBTOOL_MIN))
+$(eval $(call COMPARE_VERSIONS,pkg-config --version|head -1|$(VERSION_MASK),0.29.1,PKG_CONFIG_MIN))
 
 ifneq ($(MAKECMDGOALS),rsync)
 ifneq ($(MAKECMDGOALS),clean)
@@ -97,7 +103,6 @@ bitcoind-restart:
 include mk/common.mk
 include mk/autotools.mk
 include mk/gcc.mk
-include mk/tools.mk
 include mk/libs.mk
 include mk/bitcoind.mk
 include mk/electrumx.mk
--- mk/autotools.mk
+++ mk/autotools.mk
@@ -1,6 +1,7 @@
 # autoconf...
 autoconf-2.69.tar.gz: |\
-    $(HOME)/.bitcoin_envs
+    $(HOME)/.bitcoin_envs\
+    m4_install
 	wget 'http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz'
 	echo '82d05e03b93e45f5a39b828dc9c6c29b  autoconf-2.69.tar.gz'|md5sum --check - || \
 		{ \
@@ -60,3 +61,31 @@ libtool_install: |\
 		./configure --prefix=$(BASE_INSTALL_DIR) $(CONFIGURE_VARS) && $(MAKE_COMPILE) && $(MAKE) install && echo "Libtool was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
+
+pkg-config_install: |\
+    $(HOME)/.bitcoin_envs\
+    autotools_install
+	cd external/pkg-config && { \
+		./autogen.sh --with-internal-glib --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) install && echo "pkg-config was installed - OK"; \
+	} &> make_out.txt && tail make_out.txt
+	@touch $@
+
+# m4
+m4-1.4.18.tar.gz:
+	wget 'http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz'
+	echo 'a077779db287adf4e12a035029002d28  m4-1.4.18.tar.gz'|md5sum --check - || \
+		{ \
+			mv m4-1.4.18.tar.gz m4-1.4.18.bad.tar.gz &&\
+			echo "Bad m4 md5 sum"; false;\
+		}
+
+
+m4_install: |\
+    $(HOME)/.bitcoin_envs\
+    m4-1.4.18.tar.gz
+	tar xzf m4-1.4.18.tar.gz
+	cd m4-1.4.18 && { \
+		./configure --prefix=$(BASE_INSTALL_DIR) $(CONFIGURE_VARS) && $(MAKE_COMPILE) && $(MAKE) install && echo "The m4 was installed - OK"; \
+	} &> make_out.txt && tail make_out.txt
+	@touch $@
+
