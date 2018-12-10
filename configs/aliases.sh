c9617dc6810d46c1b90c0efe40865a3592370237
Perlover <perlover@perlover.com>
2018-12-10 19:08:41 +0100

    Added the alias 'lt' for lncli of testnet

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -1,3 +1,4 @@
 alias testnet-lncli="lncli --macaroonpath $HOME/.lnd/data/chain/bitcoin/testnet/admin.macaroon --rpcserver localhost:10010"
 alias mainnet-lncli="lncli --macaroonpath $HOME/.lnd/data/chain/bitcoin/mainnet/admin.macaroon --rpcserver localhost:10009"
 alias l="mainnet-lncli"
+alias lt="testnet-lncli"
