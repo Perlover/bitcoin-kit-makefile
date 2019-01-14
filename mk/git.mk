7977c62af5bde5363abee0fdcc47a0712c97f7f5
Perlover <perlover@perlover.com>
2019-01-14 19:52:26 +0100

    install git from tar

--- mk/git.mk
+++ mk/git.mk
@@ -1,12 +1,8 @@
-new_git_download:
-	git clone https://github.com/git/git
-	@touch $@
-
 new_git_install: |\
     required_for_configure_install\
     new_curl_install\
-    new_git_download
-	cd git && $(MAKE) configure && ./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) all && $(MAKE) install
+    git-2.20.1.tar.gz
+	cd git-2.20.1 && $(MAKE) configure && ./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) all && $(MAKE) install
 	@touch $@
 
 curl-7.63.0.tar.gz:
@@ -25,3 +21,11 @@ new_curl_install: |\
 		./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) install && echo "Curl was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
+
+git-2.20.1.tar.gz:
+	wget 'https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.20.1.tar.gz'
+	echo '7a7769e5c957364ed0aed89e6e67c254  git-2.20.1.tar.gz'|md5sum --check - || \
+		{ \
+			mv git-2.20.1.tar.gz git-2.20.1.bad.tar.gz &&\
+			echo "Bad curl md5 sum"; false;\
+		}
