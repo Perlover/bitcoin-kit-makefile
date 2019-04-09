faf6e450c50e40aa2f9c94c36d2f661dd8b51a23
Perlover <perlover@perlover.com>
2019-04-09 17:38:43 +0200

    fix again ;-)

--- mk/inotify.mk
+++ mk/inotify.mk
@@ -1,6 +1,6 @@
 inotify_install: |\
     required_for_configure_install
 	cd external/inotify-tools && { \
-		./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) install && echo "inotify-tools was installed - OK"; \
+		./autogen.sh && ./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) install && echo "inotify-tools was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
