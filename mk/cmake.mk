2dfaa599dec1f859867045e8296068e5a05bd8f8
Perlover <perlover@perlover.com>
2020-06-15 17:30:30 +0200

    fix cmake

--- mk/cmake.mk
+++ mk/cmake.mk
@@ -1,17 +1,17 @@
-cmake-3.16.8.tar.gz:
-	wget 'https://github.com/Kitware/CMake/releases/download/v3.16.8/cmake-3.16.8.tar.gz'
-	echo 'd47f23a9781b68014e77717f8e291bb7  cmake-3.16.8.tar.gz'|md5sum --check - || \
+cmake-3.17.3.tar.gz:
+	wget 'https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz'
+	echo 'd47f23a9781b68014e77717f8e291bb7  cmake-3.17.3.tar.gz'|md5sum --check - || \
 		{ \
-			mv cmake-3.16.8.tar.gz cmake-3.16.8.bad.tar.gz &&\
+			mv cmake-3.17.3.tar.gz cmake-3.17.3.bad.tar.gz &&\
 			echo "Bad cmake md5 sum"; false;\
 		}
 
 cmake_install: |\
     required_for_configure_install\
     binutils_install\
-    cmake-3.16.8.tar.gz
-	tar xzf cmake-3.16.8.tar.gz
-	cd cmake-3.16.8 && { \
+    cmake-3.17.3.tar.gz
+	tar xzf cmake-3.17.3.tar.gz
+	cd cmake-3.17.3 && { \
 		./bootstrap --parallel=4 --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) install && echo "The cmake was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
