1bc3cb5c6675c05aff1ae57493454b5994175754
Perlover <perlover@perlover.com>
2021-10-20 14:31:35 +0200

    added bison for glibc

--- mk/libs.mk
+++ mk/libs.mk
@@ -49,15 +49,30 @@ zlib_install: |\
 	@touch $@
 
 glibc-2.34.tar.gz:
-	wget --no-check-certificate 'https://ftp.gnu.org/gnu/glibc/$@' &&\
+	wget 'http://ftp.gnu.org/gnu/glibc/$@' &&\
 	echo '255b7632746b5fdd478cb7b36bebd1ec1f92c2b552ee364c940f48eb38d07f62  $@'|sha256sum --check - || { echo "Bad checksum"; false; }
 
 glibc_install: |\
     required_for_configure_install\
+    bison_install\
     glibc-2.34.tar.gz
 	tar xzf glibc-2.34.tar.gz
 	cd glibc-2.34 && { \
 		mkdir build && cd build && \
 		../configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) test && $(MAKE) install && echo "The glib was installed - OK"; \
+	} &> make_out.txt && tail make_out.txt
+	@touch $@
+
+
+bison-3.8.tar.gz:
+	wget 'http://ftp.gnu.org/gnu/bison/$@' &&\
+	echo 'd5d184d421aee15603939973a6b0f372f908edfb24c5bc740697497021ad9458  $@'|sha256sum --check - || { echo "Bad checksum"; false; }
+
+bison_install: |\
+    required_for_configure_install\
+    bison-3.8.tar.gz
+	tar xzf bison-3.8.tar.gz
+	cd bison-3.8 && { \
+		./configure --prefix=$(BASE_INSTALL_DIR) && $(MAKE_COMPILE) && $(MAKE) test && $(MAKE) install && echo "The bison was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
