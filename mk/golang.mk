8e4217091bae3945adcb0e34d4ba6650b5723383
Perlover <perlover@perlover.com>
2024-05-31 10:46:14 +0200

    fixes for golang compiling v1.22.3

--- mk/golang.mk
+++ mk/golang.mk
@@ -1,11 +1,13 @@
 # https://lore.kernel.org/buildroot/CA+h8R2rtcynkCBsz=_9yANOEguyPCOcQDj8_ns+cv8RS8+8t9A@mail.gmail.com/T/
-# New 1.20.3 requires 3-stage process :(
+# New 1.22.3 requires 4-stage process :(
 
 GOLANG_VER_STAGE_1 := 1.4
 GOLANG_VER_STAGE_2 := 1.19.5
-GOLANG_VER_STAGE_3 := $(GOLANG_VER)
+GOLANG_VER_STAGE_3 := 1.20.3
+GOLANG_VER_STAGE_4 := $(GOLANG_VER)
 
 GOLANG_TARGET_STAGE_2 := $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_2)/bin/go
+GOLANG_TARGET_STAGE_3 := $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_3)/bin/go
 
 golang_pre_install_$(GOLANG_VER_STAGE_1): |\
     required_for_configure_install\
@@ -21,15 +23,21 @@ $(GOLANG_TARGET_STAGE_2): | golang_pre_install_$(GOLANG_VER_STAGE_1)
 	-rm -rf $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_2)
 	cd $(BASE_INSTALL_DIR) && git clone --local $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) go$(GOLANG_VER_STAGE_2) && cd go$(GOLANG_VER_STAGE_2) && git checkout go$(GOLANG_VER_STAGE_2) && cd src && ulimit -u `ulimit -H -u` && GOROOT_BOOTSTRAP=$(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) ./make.bash
 
-$(CURRENT_GOLANG_TARGET): | $(GOLANG_TARGET_STAGE_2)
+$(GOLANG_TARGET_STAGE_3): | $(GOLANG_TARGET_STAGE_2)
+	cd $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) && git fetch origin
+	-rm -rf $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_2)
+	cd $(BASE_INSTALL_DIR) && git clone --local $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) go$(GOLANG_VER_STAGE_3) && cd go$(GOLANG_VER_STAGE_3) && git checkout go$(GOLANG_VER_STAGE_3) && cd src && ulimit -u `ulimit -H -u` && GOROOT_BOOTSTRAP=$(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_2) ./make.bash
+
+$(CURRENT_GOLANG_TARGET): | $(GOLANG_TARGET_STAGE_3)
 	cd $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) && git fetch origin
 	-rm -rf $(BASE_INSTALL_DIR)/go$(GOLANG_VER)
-	cd $(BASE_INSTALL_DIR) && git clone --local $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) go$(GOLANG_VER) && cd go$(GOLANG_VER) && git checkout go$(GOLANG_VER) && cd src && ulimit -u `ulimit -H -u` && GOROOT_BOOTSTRAP=$(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_2) ./make.bash
+	cd $(BASE_INSTALL_DIR) && git clone --local $(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_1) go$(GOLANG_VER) && cd go$(GOLANG_VER) && git checkout go$(GOLANG_VER) && cd src && ulimit -u `ulimit -H -u` && GOROOT_BOOTSTRAP=$(BASE_INSTALL_DIR)/go$(GOLANG_VER_STAGE_3) ./make.bash
 
 golang_envs-$(GOLANG_VER).sh: golang_envs.sh
 	cp -f $< $@
 	sed -ri -e 's#\$$\$$GOLANG_VER\$$\$$#$(GOLANG_VER)#g' $@
 	sed -ri -e 's#\$$\$$GOLANG_VER_STAGE_2\$$\$$#$(GOLANG_VER_STAGE_2)#g' $@
+	sed -ri -e 's#\$$\$$GOLANG_VER_STAGE_3\$$\$$#$(GOLANG_VER_STAGE_3)#g' $@
 	mkdir -p $(BASE_INSTALL_DIR)/go
 
 # ~/.bash_profile patch...
