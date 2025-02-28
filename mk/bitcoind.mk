e6dad0378287886d8b861193059ad7c31ef26ec3
Perlover <perlover@perlover.com>
2025-02-28 10:13:27 +0100

    Added LIBS=... to bitcoind (v28.1) options for CentOS 6.* compiling

--- mk/bitcoind.mk
+++ mk/bitcoind.mk
@@ -11,7 +11,7 @@ bitcoin-core_install: |\
     miniupnpc_install
 	cd external/bitcoin-core && git checkout -f && git clean -fdx && { \
 		./autogen.sh && \
-		./configure --prefix=$(BASE_INSTALL_DIR) $(CONFIGURE_VARS) --with-incompatible-bdb --disable-wallet --without-gui --with-boost=$(BASE_INSTALL_DIR) --with-boost-libdir=$(BASE_INSTALL_DIR)/lib && $(MAKE) -j4 && $(MAKE) install && echo "The bitcoin-core was installed - OK"; \
+		./configure LIBS=-lrt --prefix=$(BASE_INSTALL_DIR) $(CONFIGURE_VARS) --with-incompatible-bdb --disable-wallet --without-gui --with-boost=$(BASE_INSTALL_DIR) --with-boost-libdir=$(BASE_INSTALL_DIR)/lib && $(MAKE) -j4 && $(MAKE) install && echo "The bitcoin-core was installed - OK"; \
 	} &> make_out.txt && tail make_out.txt
 	@touch $@
 
